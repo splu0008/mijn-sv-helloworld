480847354366e17295d2085cf2c6e587b1707066
Thijs Clowting <thijs@clowting.me>
2016-08-28 22:56:32 +0200

    Deployment, removed fetch

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,6 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy-key.pem
 ssh-add .travis/deploy-key.pem
-git fetch --unshallow
 git remote add deploy ssh://helloworld@mijn-sv-helloworld.nl:/home/helloworld/mijn-sv-helloworld.git
 git push deploy +feature/deployment:refs/heads/master
 exit
