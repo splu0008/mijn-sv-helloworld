20f413dd7133b1038b4d0532f5845b5ffe911334
Thijs Clowting <thijs@clowting.me>
2016-08-28 22:41:22 +0200

    Deployment test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,5 @@ chmod 600 .travis/deploy-key.pem
 ssh-add .travis/deploy-key.pem
 git fetch --unshallow
 git remote add deploy ssh://helloworld@mijn-sv-helloworld.nl:/home/helloworld/mijn-sv-helloworld.git
-git push deploy +master:refs/heads/master
+git push deploy +feature/deployment:refs/heads/master
 exit
