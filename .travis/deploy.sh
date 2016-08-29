afc7b70ef9ac424c82bac0aab6267cf225ef150a
Thijs Clowting <thijs@clowting.me>
2016-08-29 20:19:38 +0200

    Remote fix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,7 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy-key.pem
 ssh-add .travis/deploy-key.pem
-git remote add deploy ssh://helloworld@mijn-sv-helloworld.nl:/home/helloworld/mijn-sv-helloworld.git
+git fetch --unshallow
+git remote add deploy ssh://helloworld@mijn.svhelloworld.nl:/home/helloworld/mijn-sv-helloworld.git
 git push deploy +feature/deployment:refs/heads/master
 exit
